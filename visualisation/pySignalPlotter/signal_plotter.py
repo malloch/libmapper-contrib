62260ce77b53311a1ad7aad3a721a0fb0a0fe9aa
Joseph Malloch <jmalloch@dal.ca>
2022-05-16 18:08:05 -0300

    Visualisation:pySignalPlotter: updates and added rough support for plotting instanced signals.

--- visualisation/pySignalPlotter/signal_plotter.py
+++ visualisation/pySignalPlotter/signal_plotter.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 import sys, math
-from PySide2 import QtGui, QtCore
-from PySide2.QtWidgets import QApplication, QMainWindow, QFrame, QSplitter
+from PySide6 import QtGui, QtCore
+from PySide6.QtWidgets import QApplication, QMainWindow, QFrame, QSplitter
 from collections import deque
-import mapper as mpr, random
+import libmapper as mpr, random
 
 display_sec = 10.0
 
@@ -17,54 +17,40 @@ cleared = 1
 
 x_scale = split / display_sec
 
-def recheck_ranges(record):
-    min = None
-    max = None
-    for i in record['vals']:
-        if math.isnan(i) or math.isinf(i):
-            continue
-        if not min or i < min:
-            min = i
-        if not max or i > max:
-            max = i
-    record['min'][0] = min
-    record['max'][0] = max
-
 def sig_handler(sig, event, id, val, tt):
     now = tt.get_double()
     then = now - display_sec
+
+    if event == mpr.Signal.Event.REL_UPSTRM:
+        sig.instance(id).release()
+    elif event == mpr.Signal.Event.INST_OFLW:
+        return
+
     # Find signal
     try:
         match = signals[sig['name']]
     except:
         print('signal not found')
         return
-    match['vals'].append(val)
-    if not math.isnan(val) and not math.isinf(val):
-        if match['min'][0] == None or val < match['min'][1]:
-            match['min'][0] = val
-        if match['max'][0] == None or val > match['max'][1]:
-            match['max'][0] = val
-    match['tts'].append(now)
-    to_pop = -1
-    for i in match['tts']:
-        if i < then:
-            to_pop += 1
-    if to_pop <= 0:
-        return
-    recheck = 0
-    for i in range(to_pop):
-        popped = match['vals'].popleft()
-        if popped <= match['min'][0] or popped >= match['max'][0]:
-            recheck = 1
-        match['tts'].popleft()
-    if recheck:
-        recheck_ranges(match)
+    if id in match['vals']:
+        match['vals'][id].append(val)
+        match['tts'][id].append(now)
+    else:
+        match['vals'][id] = deque([val])
+        match['tts'][id] = deque([now])
+        match['pen'][id] = QtGui.QPen(QtGui.QBrush(QtGui.QColor(random.randint(0,255),
+                                      random.randint(0,255), random.randint(0,255))), 2)
+    if val is not None:
+        if not math.isnan(val) and not math.isinf(val):
+            if match['min'][0] == None or val < match['min'][1]:
+                match['min'][0] = val
+            if match['max'][0] == None or val > match['max'][1]:
+                match['max'][0] = val
 
 def on_map(type, map, event):
     print('on_map!')
-    src = map.signals(mpr.LOC_SRC)[0]
-    dst = map.signals(mpr.LOC_DST)[0]
+    src = map.signals(mpr.Location.SOURCE)[0]
+    dst = map.signals(mpr.Location.DESTINATION)[0]
     print(src['name'], '->', dst['name'])
     if src['is_local']:
         map.release()
@@ -73,26 +59,29 @@ def on_map(type, map, event):
         return
     srcname = src.device()['name']+'/'+src['name']
     dstname = dst['name']
-    if event == mpr.OBJ_NEW:
+    if event == mpr.Graph.Event.NEW:
         print('  ADDED map ', srcname, '->', '<self>/'+dstname)
         if dstname == srcname:
             return
         print('  rerouting...')
-        newsig = dev.add_signal(mpr.DIR_IN, srcname, 1, mpr.FLT, None, None, None, None, sig_handler)
+        newsig = dev.add_signal(mpr.Direction.INCOMING, srcname, 1, mpr.Type.FLOAT,
+                                None, None, None, 10, sig_handler, mpr.Signal.Event.ALL)
         if not newsig:
             print('  error creating signal', srcnamefull)
             return
+        color = QtGui.QColor(random.randint(0,255), random.randint(0,255), random.randint(0,255))
         signals[srcname] = {'sig' : newsig,
-                            'vals' : deque([]),
-                            'tts' : deque([]),
-                            'len' : 0, 'min' : [None, 0],
+                            'vals' :{0: deque([])},
+                            'tts' : {0: deque([])},
+                            'min' : [None, 0],
                             'max' : [None, 1],
-                            'pen' : QtGui.QPen(QtGui.QBrush(QtGui.QColor(random.randint(0,255), random.randint(0,255), random.randint(0,255))), 2),
+                            'color' : color,
+                            'pen' : {0: QtGui.QPen(QtGui.QBrush(color), 2)},
                             'label' : 0}
         print('  signals:', signals)
-        mpr.map(src, newsig).push()
+        mpr.Map(src, newsig).push()
         map.release()
-    elif event == mpr.OBJ_REM or event == mpr.OBJ_EXP:
+    elif event == mpr.Graph.Event.REMOVED or event == mpr.Graph.Event.EXPIRED:
         print('  REMOVED', srcname, '->', '<self>/'+dstname)
         if dstname == 'connect_here' or dstname != srcname:
             return
@@ -102,9 +91,9 @@ def on_map(type, map, event):
 #            device.remove_signal(signals[dstname]['sig'])
             del signals[dstname]
 
-dev = mpr.device('signal_plotter')
-dev.add_signal(mpr.DIR_IN, 'connect_here')
-dev.graph().add_callback(on_map, mpr.MAP)
+dev = mpr.Device('signal_plotter')
+dev.add_signal(mpr.Direction.INCOMING, 'connect_here')
+dev.graph().add_callback(on_map, mpr.Type.MAP)
 
 def resize(pos, index):
     global split, x_scale
@@ -122,7 +111,7 @@ class labels(QFrame):
 
     def drawLabels(self, event, qp):
         for i in signals:
-            qp.setPen(signals[i]['pen'])
+            qp.setPen(signals[i]['pen'][0])
             #string = i + ' ( ' + str((len(signals[i]['vals']) - 1) / display_sec) + ' Hz )'
             qp.drawText(5, signals[i]['label'], i)
 
@@ -137,69 +126,83 @@ class plotter(QFrame):
         self.drawGraph(event, qp)
 
     def drawGraph(self, event, qp):
-        then = mpr.time().get_double() - display_sec   # 10 seconds ago
-        for i in signals:
-            if not len (signals[i]['vals']):
+        global x_scale
+        then = mpr.Time().get_double() - display_sec   # 10 seconds ago
+        for s in signals:
+            if not len (signals[s]['vals']):
                 continue
-            target = signals[i]['min'][0]
+            target = signals[s]['min'][0]
             if target == None:
                 continue
-            signals[i]['min'][1] *= 0.9
-            signals[i]['min'][1] += target * 0.1
-            target = signals[i]['max'][0]
-            signals[i]['max'][1] *= 0.9
-            signals[i]['max'][1] += target * 0.1
+            signals[s]['min'][1] *= 0.9
+            signals[s]['min'][1] += target * 0.1
+            target = signals[s]['max'][0]
+            signals[s]['max'][1] *= 0.9
+            signals[s]['max'][1] += target * 0.1
 
             y_offset = 0
-            y_scale = signals[i]['max'][1] - signals[i]['min'][1]
+            y_scale = signals[s]['max'][1] - signals[s]['min'][1]
             if y_scale == 0:
                 y_scale = -0.5
             else:
                 y_scale = -height / y_scale
-                y_offset = height - signals[i]['min'][1] * y_scale
+                y_offset = height - signals[s]['min'][1] * y_scale
 
             path = None
-            vals = signals[i]['vals']
-            tts = signals[i]['tts']
-            x = 0
-            y = 0
-            started = False
-            wasNan = False
             label_y = 0
             label_y_count = 0
-            tt = tts[0]
-            for j in range(len(vals)):
-                if vals[j] == None or math.isnan(vals[j]) or math.isinf(vals[j]):
-                    wasNan = True
-                    continue;
-                x = (tts[j] - then) * x_scale
-                y = vals[j] * y_scale + y_offset
-                point = QtCore.QPointF(x, y)
-                diff = tts[j] - tt
-                tt = tts[j]
-                if not started:
-                    path = QtGui.QPainterPath(point)
-                    started = True
-                else:
-                    if wasNan or diff > 1.0:
-                        path.moveTo(point)
-                    else:
-                        path.lineTo(point)
-                    if label_y_count < 3:
-                        label_y += y
-                        label_y_count += 1
+
+            for i in signals[s]['vals']:
+                tts = signals[s]['tts'][i]
+                vals = signals[s]['vals'][i]
+                to_pop = 0
+                for tt in tts:
+                    if tt > then:
+                        break
+                    to_pop += 1
+                for j in range(to_pop):
+                    vals.popleft()
+                    tts.popleft()
+
+                if not len (signals[s]['vals'][i]):
+                    continue
+                x = 0
+                y = 0
+                started = False
                 wasNan = False
-            if label_y_count:
-                label_y /= label_y_count
-            signals[i]['label'] *= 0.99
-            signals[i]['label'] += label_y * 0.01
-            qp.strokePath(path, signals[i]['pen'])
+                tt = tts[0]
+                for t in range(len(tts)):
+                    if vals[t] == None or math.isnan(vals[t]) or math.isinf(vals[t]):
+                        wasNan = True
+                        continue;
+                    x = (tts[t] - then) * x_scale
+                    y = vals[t] * y_scale + y_offset
+                    point = QtCore.QPointF(x, y)
+                    diff = tts[t] - tt
+                    tt = tts[t]
+                    if not started:
+                        path = QtGui.QPainterPath(point)
+                        started = True
+                    else:
+                        if wasNan or diff > 1.0:
+                            path.moveTo(point)
+                        else:
+                            path.lineTo(point)
+                        if label_y_count < 3:
+                            label_y += y
+                            label_y_count += 1
+                    wasNan = False
+                if label_y_count:
+                    label_y /= label_y_count
+                qp.strokePath(path, signals[s]['pen'][i])
+            signals[s]['label'] *= 0.99
+            signals[s]['label'] += label_y * 0.01
 
 class gui(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setGeometry(300, 100, width, height)
-        self.setWindowTitle('libmpr signal plotter')
+        self.setWindowTitle('libmapper signal plotter')
         self.splitter = QSplitter(QtCore.Qt.Horizontal, self)
 
         self.setCentralWidget(self.splitter)
@@ -237,9 +240,8 @@ class gui(QMainWindow):
         split = self.splitter.sizes()[0]
 
 def remove_dev():
-    print('called remove_dev')
     global dev
-    del dev
+    dev.free()
 
 import atexit
 atexit.register(remove_dev)
@@ -247,4 +249,4 @@ atexit.register(remove_dev)
 app = QApplication(sys.argv)
 gui = gui()
 gui.show()
-sys.exit(app.exec_())
+sys.exit(app.exec())
