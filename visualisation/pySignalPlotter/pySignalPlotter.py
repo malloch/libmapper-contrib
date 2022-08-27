d6d83a170a9a1946e5108f83bbbc09f68c73f8bd
Joseph Malloch <jmalloch@dal.ca>
2022-08-27 13:06:06 -0300

    pySignalPlotter: added close button, small tweaks.

--- visualisation/pySignalPlotter/pySignalPlotter.py
+++ visualisation/pySignalPlotter/pySignalPlotter.py
@@ -3,6 +3,7 @@ import pyqtgraph as pg
 import sys, math, time
 from collections import deque
 import libmapper as mpr
+import os
 
 signals = {}
 sigs_to_free = []
@@ -10,6 +11,10 @@ sigs_to_free = []
 display_sec = 10.0
 
 use_2d = False
+
+icon_path = os.getcwd() + '/clear.png'
+
+print('icon_path', icon_path)
 
 class TimeAxisItem(pg.AxisItem):
     def __init__(self, *args, **kwargs):
@@ -56,10 +61,8 @@ def sig_handler(sig, event, id, val, tt):
     match['tts'][id].append(now)
 
 def on_map(type, map, event):
-    print('on_map!')
     src = map.signals(mpr.Location.SOURCE)[0]
     dst = map.signals(mpr.Location.DESTINATION)[0]
-    print(src['name'], '->', dst['name'])
     if src['is_local']:
         map.release()
         return
@@ -68,7 +71,6 @@ def on_map(type, map, event):
     srcname = src.device()['name']+'/'+src['name']
     dstname = dst['name']
     if event == mpr.Graph.Event.NEW:
-        print('  ADDED map ', srcname, '->', '<self>/'+dstname)
         if dstname == srcname:
             return
 
@@ -79,7 +81,6 @@ def on_map(type, map, event):
             map.release()
             return
 
-        print('  rerouting...')
         vec_len = src[mpr.Property.LENGTH]
         num_inst = src[mpr.Property.NUM_INSTANCES]
         newsig = dev.add_signal(mpr.Direction.INCOMING, srcname, vec_len, mpr.Type.FLOAT,
@@ -97,11 +98,9 @@ def on_map(type, map, event):
                             'pens'      : [pg.mkPen(color=i, width=3) for i in range(vec_len)],
                             'curves'    : {},
                             'label'     : 0 }
-        print('  signals:', signals)
         mpr.Map(src, newsig).push()
         map.release()
     elif event == mpr.Graph.Event.REMOVED or event == mpr.Graph.Event.EXPIRED:
-        print('  REMOVED', srcname, '->', '<self>/'+dstname)
         if dstname == 'connect_here' or dstname != srcname:
             return
         if dstname in signals:
@@ -114,19 +113,39 @@ graph.add_callback(on_map, mpr.Type.MAP)
 dev = mpr.Device('signal_plotter', graph)
 dummy = dev.add_signal(mpr.Direction.INCOMING, 'connect_here')
 
-class Plotter(QtWidgets.QLabel):
-    def __init__(self, text):
-        super(TempLabel, self).__init__(text)
+class CloseButton(pg.ButtonItem):
+    def __init__(self, parentItem):
+        super(CloseButton, self).__init__(pg.icons.getGraphPixmap('auto'), 14, parentItem)
+
+        self.setImageFile(icon_path)
+        self.setScale(0.1)
+        self.setOpacity(1)
+
+class Plotter(pg.GraphicsLayoutWidget):
+    def __init__(self):
+        # TODO: pass in graph ref
+        super(Plotter, self).__init__(show=True)
         self.setAcceptDrops(True)
 
-    def dragEnterEvent(self, e):
-        print('dragEnterEvent', e)
-        e.accept()
+    def add_plot(self, r, c, label):
+        # TODO: pass in signal ref
+        plot = self.addPlot(row=r, col=c)
 
-    def dropEvent(self, e):
-        print('dropEvent', e)
-        e.setDropAction(QtCore.Qt.CopyAction)
-        e.accept()
+        plot.setAxisItems({'bottom': TimeAxisItem(orientation='bottom')})
+        plot.closeButton = CloseButton(plot)
+
+        plot.showGrid(x=True, y=True)
+        plot.setLabel('left', label)
+
+        # TODO: add map display?
+
+        return plot
+
+class HLine(QtWidgets.QFrame):
+    def __init__(self):
+        super(HLine, self).__init__()
+        self.setFrameShape(QtWidgets.QFrame.HLine)
+        self.setFrameShadow(QtWidgets.QFrame.Sunken)
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -146,8 +165,7 @@ class MainWindow(QtWidgets.QMainWindow):
         button_use_2d = QtWidgets.QCheckBox('Draw 2D vectors on canvas', self)
         button_use_2d.stateChanged.connect(self.update_use_2d)
 
-        self.plots = pg.GraphicsLayoutWidget(show=True)
-#        self.plots.setBackground('w')
+        self.plots = Plotter()
 
         addBelow = QtWidgets.QPushButton('+', self)
         addBelow.setStyleSheet("font: 22px; border-radius: 5")
@@ -164,18 +182,16 @@ class MainWindow(QtWidgets.QMainWindow):
         grid.addWidget(time_label, 0, 0, QtCore.Qt.AlignmentFlag.AlignLeft)
         grid.addWidget(time_spinbox, 0, 1, QtCore.Qt.AlignmentFlag.AlignLeft)
         grid.addWidget(button_use_2d, 0, 3, QtCore.Qt.AlignmentFlag.AlignRight)
-        grid.addWidget(self.plots, 1, 0, 1, 4)
-        grid.addWidget(addRight, 1, 4, 1, 1)
-        grid.addWidget(addBelow, 2, 0, 1, 4)
+        grid.addWidget(HLine(), 1, 0, 1, 4)
+        grid.addWidget(self.plots, 2, 0, 1, 4)
 
         grid.setColumnStretch(0, 0)
         grid.setColumnStretch(1, 0)
         grid.setColumnStretch(2, 1)
         grid.setColumnStretch(3, 0)
-        grid.setColumnStretch(4, 0)
         grid.setRowStretch(0, 0)
-        grid.setRowStretch(1, 2)
-        grid.setRowStretch(2, 0)
+        grid.setRowStretch(1, 0)
+        grid.setRowStretch(2, 1)
 
         centralWidget = QtWidgets.QWidget()
         centralWidget.setLayout(grid)
@@ -187,21 +203,17 @@ class MainWindow(QtWidgets.QMainWindow):
         self.timer.start()
 
     def dragEnterEvent(self, e):
-        print('dragEnterEvent', e)
         if e.mimeData().hasFormat("text/plain"):
-            print('accepting!')
             e.accept()
 
     def dropEvent(self, e):
         text = e.mimeData().text()
-        print('dropEvent', text)
 
         if text.startswith('libmapper://'):
             e.setDropAction(QtCore.Qt.CopyAction)
             e.accept()
 
             names = text[12:].split('/')
-            print('adding plot for signal', names)
 
             if len(names) != 2:
                 return
@@ -218,7 +230,6 @@ class MainWindow(QtWidgets.QMainWindow):
             if s[mpr.Property.IS_LOCAL]:
                 print('error: plotting local signals is not allowed')
                 return
-            print('starting dummy map!')
             mpr.Map(s, dummy).push()
 
         else:
@@ -249,6 +260,9 @@ class MainWindow(QtWidgets.QMainWindow):
     def add_plot_right(self):
         print('add plot right!')
 
+    def closePlot(self, signame):
+        sigs_to_free.append(signals.pop(signame))
+
     def update_graph(self):
         now = mpr.Time().get_double()
         then = now - display_sec   # 10 seconds ago
@@ -261,17 +275,8 @@ class MainWindow(QtWidgets.QMainWindow):
             if 'plot' in data:
                 plot = data['plot']
             else:
-                plot = self.plots.addPlot(row=self.plots.nextRow(), col=0)
-                plot.setAxisItems({'bottom': TimeAxisItem(orientation='bottom')})
-
-                units = data['sig'][mpr.Property.UNIT]
-                if units == 'unknown':
-                    units = ''
-                else:
-                    units = ' (' + units + ')'
-                plot.setLabel('left', data['sig'][mpr.Property.NAME] + units)
-
-                plot.showGrid(x=True, y=True)
+                plot = self.plots.add_plot(self.plots.nextRow(), 0, data['sig'][mpr.Property.NAME])
+                plot.closeButton.clicked.connect(lambda: self.closePlot(s))
                 data['plot'] = plot
 
             updated = False
@@ -293,7 +298,7 @@ class MainWindow(QtWidgets.QMainWindow):
                 for j in range(to_pop):
                     tts.popleft()
 
-                if not len (data['tts'][inst]):
+                if not len(tts):
                     continue
 
                 if signals[s]['curves'][inst] == None:
@@ -311,8 +316,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
             if vec_len != 2 or use_2d == False:
                 plot.setXRange(then, now)
-
-            if not updated:
+            elif not updated:
                 signals[s]['plot'].clear()
 
     def timer_event(self):
